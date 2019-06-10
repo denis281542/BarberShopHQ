a17742606357a241054cf29bf24f0341fcc08629
denis281542 <deniskuimov3@gmail.com>
2019-06-10 06:58:21 +0300

    Add Rakefile

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ require 'sinatra/activerecord'
 #подключение к БД
 set :datebase, "sqlite3:barbershop.db"
 
+#задаем сущность
+class Client < ActiveRecord::Base 
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
